3b01eaf9620ae150ee9ae43ccbc22fa0d53f1c88
SaifulShihab <shihabmd1970@gmail.com>
2019-11-20 22:57:30 +0600

    add headmaster_account table in admin

--- initapp/admin.py
+++ initapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import headmaster_account
 # Register your models here.
+admin.site.register(headmaster_account)
--- officer/admin.py
+++ officer/admin.py
@@ -2,4 +2,3 @@ from django.contrib import admin
 from .models import officer_account
 # Register your models here.
 admin.site.register(officer_account)
-
