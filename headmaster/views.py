78c60df801c1a3c14be780dfc86d83e19152ac98
Abdulla-Saimun <abdullah35-1921@diu.edu.bd>
2019-11-26 22:23:13 +0600

    about school

--- headmaster/views.py
+++ headmaster/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard(request):
     return render(request, 'headmaster/dashboard.html')
+def about(request):
+    return render(request, 'about.html')
