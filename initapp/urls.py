caf889f9a37897bd262cb4ed729cb8358c35d7b6
SaifulShihab <shihabmd1970@gmail.com>
2019-11-26 20:01:47 +0600

    add about url

--- initapp/urls.py
+++ initapp/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 urlpatterns = [
     path('', views.index, name="Index"),
     path('home', views.home, name="home"),
+    path('about', views.about, name="about"),
     path('userlogin', views.userlogin, name="userlogin"),
     path('usersignup', views.usersignup, name="usersignup"),
     path('userbacklogin', views.userbacklogin, name="userbacklogin"),
