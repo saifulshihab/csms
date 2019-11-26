5012cec849fb3d13d4757b9577309d66341900b9
Abdulla-Saimun <abdullah35-1921@diu.edu.bd>
2019-11-26 22:23:42 +0600

    about school path

--- headmaster/urls.py
+++ headmaster/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-       path('', views.dashboard, name='dashboard')
+       path('', views.dashboard, name='dashboard'),
+       path('about', views.about, name='about')
+
 ]
