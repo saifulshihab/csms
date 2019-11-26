088c01cfb313583a40e1babf48ba786a3d0d1553
SaifulShihab <shihabmd1970@gmail.com>
2019-11-27 00:10:36 +0600

    add head_logout path

--- headmaster/urls.py
+++ headmaster/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-       path('', views.dashboard, name='dashboard')
+    path('', views.dashboard, name='dashboard'),
+    path('head_logout', views.head_logout, name="head_logout"),    
 ]
