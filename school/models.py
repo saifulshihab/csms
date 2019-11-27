ed1a871e7ab15f872d28f882dd2c5eb6a9213578
SaifulShihab <shihabmd1970@gmail.com>
2019-11-27 20:18:19 +0600

    Change schoolInfo model

--- school/models.py
+++ school/models.py
@@ -1,10 +1,31 @@
 from django.db import models
-
+from django import forms
 # Create your models here.
+
 class schoolInfo(models.Model):
-    schoolId = models.IntegerField(blank=False, null=False)
-    schoolName = models.CharField(max_length=100, blank=False, null=False)
+    school_select = [
+    ('Government Laboratory High School', 'Government Laboratory High School'),
+    ('Viqarunnisa Noon School', 'Viqarunnisa Noon School'),
+    ('Rifles Public School and College', 'Rifles Public School and College'),
+    ('Dhaka Residential Model School and College',
+     'Dhaka Residential Model School and College'),
+    ('Rajuk Uttara Model School & College', 'Rajuk Uttara Model School & College'),
+    ('Dhanmondi Government Boys’ High School',
+     'Dhanmondi Government Boys’ High School'),
+    ('Saint Joseph Higher Secondary School',
+     'Saint Joseph Higher Secondary School'),
+    ('St. Gregory’s High School', 'St. Gregory’s High School'),
+    ('Motijheel Government Boys High school',
+     'Motijheel Government Boys High school'),
+    ('Mirpur Govt. High School', 'Mirpur Govt. High School'),
+    ('Django', 'Django')
+]
+    SchoolEIIN = models.CharField(
+        primary_key=True, max_length=25, blank=False, null=False)
+    schoolName = models.CharField(max_length=300, blank=False, null=False,choices=school_select )
     schoolAddress = models.CharField(max_length=100, blank=False, null=False)
-    SchoolEIIN = models.CharField(max_length=25, blank=False, null=False)
-    totalStudent = models.IntegerField()
-    totalTeacher = models.IntegerField()
+    totalStudent = models.IntegerField(blank=False, null=False)
+    totalTeacher = models.IntegerField(blank=False, null=False)
+    attendance_percentage = models.IntegerField(
+        blank=False, null=False, default=1)
+    pass_percentage = models.IntegerField(blank=False, null=False, default=1)
