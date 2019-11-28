8ecf50cc1e4d25e58b20b11d7e140dad2146927e
SaifulShihab <shihabmd1970@gmail.com>
2019-11-28 14:06:04 +0600

    show pass percentage in school detail

--- student/urls.py
+++ student/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('loginsuccess/', views.loginsuccess, name="loginsuccess"),
-    path('', views.send_feedback, name="send_feedback")
+    path('loginsuccess/', views.send_feedback, name="loginsuccess"),    
 ]
