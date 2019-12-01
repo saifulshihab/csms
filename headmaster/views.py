3fd81105683b67d7430ccb19e6f180fd158172df
Abdulla Al Saimun <45665457+Abdulla-Saimun@users.noreply.github.com>
2019-12-01 21:24:17 +0600

    headmaster views

--- headmaster/views.py
+++ headmaster/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from initapp.models import teacher_verify, headmaster_account
+
 
 # Create your views here.
 
@@ -16,6 +18,15 @@ def head_logout(request):
     except KeyError:
         pass
     return redirect('home')
-    
+
+
 def about(request):
     return render(request, 'about.html')
+
+
+def teacherverification(request):
+    s_eiin = headmaster_account.objects.get(h_empid = request.session.get('headmaster_eid'))
+    check_ein = s_eiin.sch_eiin
+    tv = teacher_verify.objects.filter(sch_eiin=check_ein)
+    #print(tv.values)
+    return render(request, 'headmaster/teacher_verification.html', {'tv': tv})
