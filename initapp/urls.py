e437aff39d1b22538edbbfd6aa2d0fb545837264
SaifulShihab <shihabmd1970@gmail.com>
2020-07-07 11:23:48 +0600

    move all func from init app

--- initapp/urls.py
+++ initapp/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="Index"),   
-    path('about', views.about, name="about"),
-    path('userlogin', views.userlogin, name="userlogin"),
-    path('usersignup', views.usersignup, name="usersignup"),
-    path('userbacklogin', views.userbacklogin, name="userbacklogin"),
-    path('userregistration', views.userregistration, name="userregistration")
+    path('home', views.home, name="home"),
+    path('about', views.about, name="about")
 ]
